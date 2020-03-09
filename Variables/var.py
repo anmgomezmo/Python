284262dc74d1c0892958adf3ae399a5e80d8e574
Slackware Live User <live@darkstar.example.net>
2020-03-09 11:57:03 -0700

    Added variables

---
+++ Variables/var.py
@@ -0,0 +1,12 @@
+i = 5
+print(i)
+i = i + 1
+print(i)
+
+s = '''This is a multi-line string.
+This is a second line.'''
+print(s)
+
+ print('Value is ', i) #Error of indentation 
+
+print('I repeat, the value is ', i)
