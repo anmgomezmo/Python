5c311bbcf7db6bd6e784ddeb841344c8b351f581
Slackware Live User <live@darkstar.example.net>
2020-03-09 11:32:16 -0700

    Modified files

--- Strings/formatmethod.py
+++ Strings/formatmethod.py
@@ -4,3 +4,6 @@ name = 'Andres'
 print('{0} is {1} years old'.format(name,age))
 print('Why is {0} playing with the python?'.format(name))
 
+print('{name} wrote {book}'.format(name='Swaroop', book='A byte of python'))
+
+
