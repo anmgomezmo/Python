324d04bb17d88c1c5dbfa95e84c0764c3f44f697
Slackware Live User <live@darkstar.example.net>
2020-03-09 10:59:08 -0700

    Modified files

--- Strings/single-doublequotes.py
+++ Strings/single-doublequotes.py
@@ -1,3 +1,13 @@
 print('Who are you?')
 
 print("What's your name")
+
+#print('What's your name') #this is an error
+
+print('What\'s your name') #this is correct
+
+#print("What"s your name")
+
+print("What\"s your name")
+
+print('Hello world \nMy name is Andres \tWhat\'s your name')
