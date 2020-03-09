2f221cb3bb0592e3909175cbec97aebd74ea6538
Slackware Live User <live@darkstar.example.net>
2020-03-09 11:05:40 -0700

    Modified files

---
+++ Strings/triplequotes.py
@@ -0,0 +1,13 @@
+print('''Helo world from the Soacha, Colombia''')
+
+#print('''How are you?
+#I'm very fine, What's your name?  #this is an error 
+#I'''m Andres and you ''')
+
+print('''How are you? 
+I'm very fine, What's your name?
+I\'''m Andres and you ''')
+
+print('''How are you? \n
+I'm very fine,\t\tWhat's your name?
+I\'''m Andres and you ''')
