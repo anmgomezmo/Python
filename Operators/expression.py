1680d96a2d0a48b32d9930fa51fcd3c88bc55a2e
Slackware Live User <live@darkstar.example.net>
2020-03-19 10:23:48 -0700

    Modified files

--- Operators/expression.py
+++ Operators/expression.py
@@ -5,8 +5,21 @@ area = length * breadth
 print('Area is', area)
 print('Perimeter is',  2 * (length + breadth))
 
-print(area ** 2)
-print(area / 3)
-print(area // 3)
-print(area % 3)
+print('Area al cuadrado',area ** 2)
+print('Area dividido 3',area / 3)
+print('Area dividido 3 parte entera',area // 3)
+print('Residuo de area dividido 3',area % 3)
 
+print(1 << area)
+print(1 >> area)
+
+area2 = area
+print(area2)
+
+x = 'str'
+y = 'stR'
+print(x == y)
+
+x = 'str'
+y = 'str'
+print(x == y)
