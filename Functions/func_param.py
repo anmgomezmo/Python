c075d5279222abbd09a25fd1b7db7831449baf66
Andres M. Gomez M <anmgomezmo@unal.edu.co>
2020-08-21 12:01:52 -0500

    Added function files examples

---
+++ Functions/func_param.py
@@ -0,0 +1,19 @@
+def print_max(a,b):
+    if a > b:
+        print(a," is maximun")
+    elif a == b:
+        print(a," is equal to ", b)
+    else:
+        print(a," is minimun")
+
+print_max(3,4)
+
+x = 5
+y = 5
+
+print_max(x,y)
+
+x = 7
+y = 2
+
+print_max(x,y)
